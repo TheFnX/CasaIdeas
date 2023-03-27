be83bd28813f29af3eac719bc379cbd928f77d95
TheFnX <beto.dubal19@gmail.com>
2023-03-27 02:15:22 -0400

    Actualización

--- src/main/java/pages/SearchOnEcommercePage.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class SearchOnEcommercePage extends BasePage{
-    public SearchOnEcommercePage(WebDriver webDriver) {
-        super(webDriver);
-    }
-}
